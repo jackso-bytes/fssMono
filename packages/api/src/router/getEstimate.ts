1dd339084cf242525e1b7e6607b06a7266260ae5
Tom0901 <tom.s.jackson090@gmail.com>
2024-06-17 10:03:43 +0100

    feat: :construction: add outline of openai call

--- packages/api/src/router/getEstimate.ts
+++ packages/api/src/router/getEstimate.ts
@@ -1,5 +1,6 @@
 // packages/api/src/trpc/router/getEstimate.ts
 import type { TRPCRouterRecord } from "@trpc/server";
+import OpenAI from "openai";
 import { z } from "zod";
 
 import { publicProcedure } from "../trpc";
@@ -12,10 +13,41 @@ export const getEstimateRouter = {
       }),
     )
     .query(async ({ input }) => {
-      const productRes = await fetch(
-        `${process.env.WORLD_OPEN_FOOD_FACTS_BASE_URL}/api/v0/product/${input.barCodeUniqueId}.json`,
-      ).then((res) => res.json());
+      // world open food facts call
+      try {
+        const productRes = await fetch(
+          `${process.env.WORLD_OPEN_FOOD_FACTS_BASE_URL}/api/v0/product/${input.barCodeUniqueId}.json`,
+        ).then((res) => res.json());
 
-      return { productRes };
+        // openai call
+        const openai = new OpenAI({
+          organization: `${process.env.OPEN_AI_ORG_ID}`,
+          project: `${process.env.OPEN_AI_PROJECT_ID}`,
+        });
+
+        const prompt = `Provide an estimated carbon footprint, sustainability score, and seasonal availability for the following food product:\n\nProduct: ${productRes.product.brands}\n\nOutput the result in the following JSON format:\n{\n  "carbonEstimate": <estimated_carbon_footprint_per_kg>,\n  "sustainability": <sustainability_score_out_of_10>,\n  "inSeason": "<season_months>"\n}`;
+
+        const gptResponse = await openai.chat.completions.create({
+          model: "text-davinci-003",
+          messages: [{ role: "user", content: prompt }],
+          max_tokens: 150,
+        });
+
+        const productInfo = gptResponse.choices[0]?.message.content?.trim();
+        const productInfoJson = productInfo ? JSON.parse(productInfo) : null;
+
+        return { productInfoJson };
+      } catch (error) {
+        console.error(
+          "Error fetching from openai or world open food facts",
+          error,
+        );
+        return {
+          error: {
+            message: "Error fetching from openai or world open food facts",
+            error,
+          },
+        };
+      }
     }),
 } satisfies TRPCRouterRecord;
