8edf96424058d43ac648744a08501ce598c15569
Tom0901 <tom.s.jackson090@gmail.com>
2024-06-22 21:53:35 +0100

    feat: :sparkles: strongly type api returns

--- packages/api/src/router/getEstimate.ts
+++ packages/api/src/router/getEstimate.ts
@@ -1,8 +1,10 @@
 // packages/api/src/trpc/router/getEstimate.ts
 import type { TRPCRouterRecord } from "@trpc/server";
+import type { ChatCompletion } from "openai/resources/index.mjs";
 import OpenAI from "openai";
 import { z } from "zod";
 
+import type { GptProductInfoJson, WorldFoodFactsProductInfo } from "../types";
 import { publicProcedure } from "../trpc";
 
 export const getEstimateRouter = {
@@ -14,36 +16,50 @@ export const getEstimateRouter = {
     )
     .query(async ({ input }) => {
       const openai = new OpenAI({
-        apiKey: `${process.env.OPEN_AI_API_KEY}`,
-        organization: `${process.env.OPEN_AI_ORG_ID}`,
-        project: `${process.env.OPEN_AI_PROJECT_ID}`,
+        apiKey: process.env.OPEN_AI_API_KEY,
+        organization: process.env.OPEN_AI_ORG_ID,
+        project: process.env.OPEN_AI_PROJECT_ID,
       });
-      // world open food facts call
+
       try {
-        const productRes = await fetch(
-          `${process.env.WORLD_OPEN_FOOD_FACTS_BASE_URL}/api/v0/product/${input.barCodeUniqueId}.json`,
-        ).then((res) => res.json());
-        const prompt = `Provide an estimated carbon footprint, sustainability score, and seasonal availability for the following food product:\n\nProduct: ${productRes.product.brands}\n\nOutput the result in the following JSON format:\n{\n  "carbonEstimate": <estimated_carbon_footprint_per_kg>,\n  "sustainability": <sustainability_score_out_of_10>,\n  "inSeason": "<season_months>"\n}`;
+        const WorldFoodFactsProductInfo = (await fetch(
+          `${process.env.WORLD_OPEN_FOOD_FACTS_BASE_URL}/api/v0/product/${input.barCodeUniqueId}.json?fields=product_name,ecoscore_data`,
+        ).then((res) => res.json())) as WorldFoodFactsProductInfo;
 
-        if (productRes.product.ecoscore_data) return productRes;
+        // early return to save api calls to gpt while testing
+        if (WorldFoodFactsProductInfo.product.ecoscore_data)
+          return {
+            gptProductInfoJson: {
+              carbonEstimate: 0,
+              sustainability: 0,
+              seasonality: "We can't tell if this product is in season",
+            },
+            WorldFoodFactsProductInfo,
+          };
 
-        const gptResponse = await openai.chat.completions.create({
-          model: "gpt-3.5-turbo",
-          messages: [{ role: "user", content: prompt }],
-          max_tokens: 150,
-        });
+        const prompt = `Provide an estimated carbon footprint, sustainability score, and seasonal availability for the following food product:\n\nProduct: ${WorldFoodFactsProductInfo.product.product_name}\n\nOutput the result in the following JSON format:\n{\n  "carbonEstimate": <estimated_carbon_footprint_per_kg>,\n  "sustainability": <sustainability_score_out_of_10>,\n  "inSeason": "<season_months>"\n}`;
 
-        const productInfo = gptResponse.choices[0]?.message.content?.trim();
-        const productInfoJson = productInfo ? JSON.parse(productInfo) : null;
-        return { productInfoJson };
-      } catch (error) {
+        const gptResponse: ChatCompletion =
+          await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: prompt }],
+            max_tokens: 150,
+          });
+
+        const gptProductInfo = gptResponse.choices[0]?.message.content?.trim();
+        const gptProductInfoJson = gptProductInfo
+          ? (JSON.parse(gptProductInfo) as GptProductInfoJson)
+          : null;
+        return { gptProductInfoJson, WorldFoodFactsProductInfo };
+      } catch (error: unknown) {
         console.error(
-          "Error fetching from openai or world open food facts",
+          "Error fetching from OpenAI or World Open Food Facts",
           error,
         );
+
         return {
           error: {
-            message: "Error fetching from openai or world open food facts",
+            message: "Error fetching from OpenAI or World Open Food Facts",
             error,
           },
         };
