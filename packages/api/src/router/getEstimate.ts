6c12df46517bfffd1101da57ee50e6ed5512f1c0
Tom0901 <tom.s.jackson090@gmail.com>
2024-06-22 13:43:49 +0100

    feat: :sparkles: refactot openai call to use gpt-3.5-turbo

--- packages/api/src/router/getEstimate.ts
+++ packages/api/src/router/getEstimate.ts
@@ -13,29 +13,28 @@ export const getEstimateRouter = {
       }),
     )
     .query(async ({ input }) => {
+      const openai = new OpenAI({
+        apiKey: `${process.env.OPEN_AI_API_KEY}`,
+        organization: `${process.env.OPEN_AI_ORG_ID}`,
+        project: `${process.env.OPEN_AI_PROJECT_ID}`,
+      });
       // world open food facts call
       try {
         const productRes = await fetch(
           `${process.env.WORLD_OPEN_FOOD_FACTS_BASE_URL}/api/v0/product/${input.barCodeUniqueId}.json`,
         ).then((res) => res.json());
-
-        // openai call
-        const openai = new OpenAI({
-          organization: `${process.env.OPEN_AI_ORG_ID}`,
-          project: `${process.env.OPEN_AI_PROJECT_ID}`,
-        });
-
         const prompt = `Provide an estimated carbon footprint, sustainability score, and seasonal availability for the following food product:\n\nProduct: ${productRes.product.brands}\n\nOutput the result in the following JSON format:\n{\n  "carbonEstimate": <estimated_carbon_footprint_per_kg>,\n  "sustainability": <sustainability_score_out_of_10>,\n  "inSeason": "<season_months>"\n}`;
 
+        if (productRes.product.ecoscore_data) return productRes;
+
         const gptResponse = await openai.chat.completions.create({
-          model: "text-davinci-003",
+          model: "gpt-3.5-turbo",
           messages: [{ role: "user", content: prompt }],
           max_tokens: 150,
         });
 
         const productInfo = gptResponse.choices[0]?.message.content?.trim();
         const productInfoJson = productInfo ? JSON.parse(productInfo) : null;
-
         return { productInfoJson };
       } catch (error) {
         console.error(
