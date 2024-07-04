8f94bda5c46785e6b26437053119fba0256afe3d
Tom0901 <tom.s.jackson090@gmail.com>
2024-07-04 17:26:04 +0100

    feat: :sparkles: custom header

---
+++ apps/expo/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Image, Text, View } from "react-native";
+
+const Header = () => {
+  return (
+    <View className="h-16 w-full flex-row items-center justify-start bg-primary pl-2">
+      <Image
+        source={require("../../assets/fssCircle.png")} // Use require() for local images
+        className="h-12 w-12"
+      />
+      <Text className="font-bold text-secondary">FSS</Text>
+    </View>
+  );
+};
+
+export default Header;
