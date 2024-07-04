8ec29ae1086dfd161597bf50c6a97f059c2a5303
Tom0901 <tom.s.jackson090@gmail.com>
2024-07-04 17:24:20 +0100

    refactor: :art: add safeareaview at the layout lvl

--- apps/expo/src/app/(tabs)/summary.tsx
+++ apps/expo/src/app/(tabs)/summary.tsx
@@ -1,21 +1,15 @@
 import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Stack } from "expo-router";
 
 export const Summary = () => {
   return (
-    <SafeAreaView className="bg-background">
-      {/* Changes page title visible on the header */}
-      <Stack.Screen options={{ title: "Summary" }} />
-      <View className="h-full w-full bg-background p-4">
-        <Text
-          className="text-8 px-2 pb-2
+    <View className="h-full w-full bg-background p-4">
+      <Text
+        className="text-8 px-2 pb-2
         pt-8"
-        >
-          Summary
-        </Text>
-      </View>
-    </SafeAreaView>
+      >
+        Summary
+      </Text>
+    </View>
   );
 };
 
