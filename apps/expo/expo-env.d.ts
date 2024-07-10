87e27a942c23a3a31d17174d81fc19cd6d31d0cd
Tom0901 <tom.s.jackson090@gmail.com>
2024-07-10 07:13:19 +0100

    feat: :sparkles: allow import of images by adding image module declarations

--- apps/expo/expo-env.d.ts
+++ apps/expo/expo-env.d.ts
@@ -1,3 +1,12 @@
 /// <reference types="expo/types" />
+declare module "*.jpg" {
+  const content: number;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: number;
+  export default content;
+}
 
 // NOTE: This file should not be edited and should be in your git ignore
