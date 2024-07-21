e5b17c69a313dd17a06e20e3e9177b01719ebf48
jackso-bytes <tom.jackson@cancer.org.uk>
2024-07-21 16:19:36 +0100

    feat: :sparkles: add prod baseUrl

--- apps/expo/src/utils/api.tsx
+++ apps/expo/src/utils/api.tsx
@@ -30,7 +30,7 @@ const getBaseUrl = () => {
   const localhost = debuggerHost?.split(":")[0];
 
   if (!localhost) {
-    // return "https://turbo.t3.gg";
+    return "https://fssmono.onrender.com";
     throw new Error(
       "Failed to get localhost. Please point to your production server.",
     );
