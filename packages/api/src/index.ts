8c964809dbd7fe8b43c0e2ad09a4af2705da2934
jackso-bytes <tom.jackson@cancer.org.uk>
2024-07-21 08:53:35 +0100

    fix: :bug:

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,7 +2,7 @@ import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 
 import type { AppRouter } from "./root";
 import { appRouter } from "./root.js";
-import { createCallerFactory, createTRPCContext } from "./trpc";
+import { createCallerFactory, createTRPCContext } from "./trpc.js";
 
 /**
  * Create a server-side caller for the tRPC API
